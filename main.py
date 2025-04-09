55b1a9c97492af3e2b19d1f6bb937c71febb0aef
Marc Pape <marc.pape@pape-development.com>
2025-04-09 18:15:01 +0200

    Added and included .env file for dir path on laptop and on home pc

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from program import Program
+from dotenv import dotenv_values
 import sys
 import os
+
+config = dotenv_values(".env")
 
 def main():
 
     laptop = True
 
     if laptop:
-        project_dir = 'C:/Users/marcp/Documents/GitHub/LegoObjectDetection'
+        project_dir = config['PROJECT_DIR_LAPTOP']
     else: 
-        project_dir = 'C:/Users/marcp/SynologyDrive/FHDW/Unterrichtsmaterial/Semester 6/Projekt Algorithmen/task_solution/LegoObjectDetection'
+        project_dir = config['PROJECT_DIR_HOME_PC']
 
     if os.getcwd() != project_dir:
         os.chdir(project_dir)
