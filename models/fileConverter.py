299a38edd8de9ccd6498b4f3b3255737e2148651
Marc Pape <marc.pape-info@web.de>
2025-04-14 19:20:09 +0200

    commentary fileConverter

--- models/fileConverter.py
+++ models/fileConverter.py
@@ -1,10 +1,17 @@
 from models.dataclasses import ColoredShape
 
+# Convert a dictionary of ROIs into a list of ColoredShape objects
 def convertRoiDictIntoColoredShapeList(dict):
     coloredShapes = []
+    
+    # Iterate through the dictionary, where keys represent colors and values are lists of ROIs
     for color, rois in dict.items():
         for roi in rois:
-            x, y, w, h = roi
+            
+            # Create a new ColoredShape object with default values for position, shapeType, and angle
             newShape = ColoredShape(None, roi, color, None, None)
+            
+            # Add the new shape to the list
             coloredShapes.append(newShape)
-    return coloredShapes
+    
+    return coloredShapes  # Return the list of ColoredShape objects
